bd73eef21b67f95c10edc143427e868397abb4a5
Esther <ueagbaji@wpi.edu>
2020-07-26 17:20:54 -0400

    more clean up

--- script.js
+++ script.js
@@ -1,60 +1,26 @@
 const quoteContainer = document.getElementById('quote-container');
-const quoteText= document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
 const googleBtn = document.getElementById('google');
-const newQuoteBtn = document.getElementById('new-quote');
 const newVerseBtn = document.getElementById('new-verse');
 const bibleRef = document.getElementById('bible');
 const verseText = document.getElementById('verse');
 const loader = document.getElementById('loader');
 
-// Show loading
-function loading() {
+function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-//Hide Loading
-function complete() {
+function removeLoadingSpinner() {
     if(!loader.hidden) {
         quoteContainer.hidden = false;
         loader.hidden = true;
     }
 }
 
-// Get Quote from API using async fetch method
-async function getQuote() {
-    //Adding a proxy url to help get through the CORs policy error
-    const proxyUrl = 'https://morning-waters-31503.herokuapp.com/'
-    const apiUrl ='http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    try {
-        const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        //If author is blank, add 'Unknown'
-        if(data.quoteAuthor === ''){
-            authorText.innerText = 'Unknown';
-        } else {
-            authorText.innerText = data.quoteAuthor;
-        }
-
-        //Reduce font size for long quote
-        if(data.quoteText.length > 120){
-            quoteText.classList.add('long-quote');
-        } else {
-            quoteText.classList.remove('long-quote')
-        }
-        quoteText.innerText = data.quoteText;
-        
-    }  catch (error) {
-        getQuote();
-        //console.log("Whoops! no quote", error);
-    }
-}
 //Get bible verse from API
 async function getBibleVerse() {
     //Show Loader
-    loading();
+    showLoadingSpinner();
 
     //Adding a proxy url to help get through the CORs policy error
     const proxyUrl = 'https://morning-waters-31503.herokuapp.com/'
@@ -67,7 +33,7 @@ async function getBibleVerse() {
   
         bibleRef.innerHTML = ref;
         verseText.innerText = results.text;
-        
+
         //Reduce font size for long quote
         if(results.text.length > 120){
             verseText.classList.add('long-quote');
@@ -75,7 +41,7 @@ async function getBibleVerse() {
             verseText.classList.remove('long-quote')
         }
         //Stop loader
-        complete();
+        removeLoadingSpinner();
     }  catch (error) {
         getBibleVerse();
         console.log("Whoops! no quote", error);
@@ -84,10 +50,10 @@ async function getBibleVerse() {
 
 
 //tweet Quote
-function tweetQuote() {
+function tweetVerse() {
     const quote = quoteText.innerText;
     const author = authorText.innerText;
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${verse} - ${text}`;
     window.open(twitterUrl, '_blank');
 }
 
@@ -100,11 +66,8 @@ function addToGoogleCalendar() {
 }
 
 //Event listeners
-//newQuoteBtn.addEventListener('click', getQuote);
 newVerseBtn.addEventListener('click', getBibleVerse);
-twitterBtn.addEventListener('click', tweetQuote);
 googleBtn.addEventListener('click', addToGoogleCalendar);
 
 //On Load
-//getQuote();
 getBibleVerse();
